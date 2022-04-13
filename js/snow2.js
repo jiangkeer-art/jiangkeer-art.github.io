1a451955558a37317d8fc21302405dc1953c216f
jiangkeer-art <130060060@qq.com>
2022-04-13 16:43:25 +0800

    Site updated: 2022-04-13 16:43:25

--- js/snow2.js
+++ js/snow2.js
@@ -3,9 +3,9 @@
 function snowFall(snow) {
     /* 可配置属性 */
     snow = snow || {};
-    this.maxFlake = snow.maxFlake || 200; /* 最多片数 */
+    this.maxFlake = snow.maxFlake || 100; /* 最多片数 */
     this.flakeSize = snow.flakeSize || 10; /* 雪花形状 */
-    this.fallSpeed = snow.fallSpeed || 1; /* 坠落速度 */
+    this.fallSpeed = snow.fallSpeed || 0.5; /* 坠落速度 */
 }
 /* 兼容写法 */
 requestAnimationFrame = window.requestAnimationFrame ||
